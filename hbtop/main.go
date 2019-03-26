41bcb9d7794f54b2eb186cdc7ff063ad921d4f19
gpmn <gpmnlxdw@eden.com>
2019-03-26 20:08:31 +0800

    use ht instead of usdt

--- hbtop/main.go
+++ hbtop/main.go
@@ -14,22 +14,29 @@ import (
 func main() {
 	fmt.Printf(`
 应朋友要求写的，我没测试过，也不保证能用。
-用于帮着大家抢TOP，每秒抢10次，每次抢100$。
+用于帮着大家抢TOP，每秒抢10次，每次抢100$。不要多开，因为限流10s 100次请求。
 使用前需要到火币开通api/secret，api/secret只给交易权限即可。
 最好自己编译，最好假设我提供的exe有安全问题。
 
+必须传入api secret参数。默认是市价单用ht买入100ht的top。
+
 用法如下：
 	./hbtop -api xxxxx -sec xxxxxx 
+
+此外还有quote,base,amt三个参数,比如上述命令等于：
+	./hbtop -api xxxxx -sec xxxxxx -base top -quote ht -amt 100
+
 测试的话，钱别放多了，要不然很快买满：
-	./hbtop -api xxxxx -sec xxxxxx -base btc -usd 10
-通常来说能够买入btc也就能买top了。
+	./hbtop -api xxxxx -sec xxxxxx -base btc -quote usdt -usd 10
+
+通常来说测试买入btcOk，也就能买top了。
 `)
 
 	api := flag.String("api", "", "你的交易api")
 	secret := flag.String("sec", "", "你的交易secret")
-	usd := flag.Float64("usd", 100.0, "按USD单次买入数量,默认100$")
+	amt := flag.Float64("amt", 100.0, "按quote单次买入数量,默认100$")
 	base := flag.String("base", "top", "想买入的品种，默认top")
-	quote := flag.String("quote", "usdt", "计价币种，默认usdt")
+	quote := flag.String("quote", "ht", "计价币种，默认ht")
 
 	flag.Parse()
 	if *api == "" || *secret == "" {
@@ -46,7 +53,7 @@ func main() {
 		time.Sleep(time.Millisecond * 100)
 		ret, err := hbex.OrderPlace(&proto.OrderPlaceParams{
 			Price:           0,
-			Amount:          *usd,
+			Amount:          *amt,
 			BaseCurrencyID:  *base,  // E.g. btc
 			QuoteCurrencyID: *quote, // e.g. usdt
 			Type:            "buy-market",
